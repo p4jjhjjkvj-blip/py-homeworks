62c20dac8b28eb8c07785813726ade2c90034d79
p4jjhjjkvj-blip <lexa_2000@mail.ru>
2026-06-13 20:15:40 +0300

    Добавлена оценка лекторам

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecture(self, lecturer, course, grade):
+        if (
+                isinstance(lecturer, Lecturer)
+                and course in self.courses_in_progress
+                and course in lecturer.courses_attached
+        ):
+            if course in lecturer.grades:
+                lecturer.grades[course].append(grade)
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 class Mentor:
     def __init__(self, name, surname):
@@ -15,16 +27,36 @@ class Mentor:
         self.courses_attached = []
 
 class Lecturer(Mentor):
-    pass
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
 
 class Reviewer(Mentor):
-    pass
+    def rate_hw(self, student, course, grade):
+        if (
+                isinstance(student, Student)
+                and course in self.courses_attached
+                and course in student.courses_in_progress
+        ):
+            if course in student.grades:
+                student.grades[course].append(grade)
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 lecturer = Lecturer('Иван', 'Иванов')
 reviewer = Reviewer('Пётр', 'Петров')
+student = Student('Алёхина', 'Ольга', 'Ж')
 
-print(isinstance(lecturer, Mentor))
-print(isinstance(reviewer, Mentor))
+student.courses_in_progress += ['Python', 'Java']
 
-print(lecturer.courses_attached)
-print(reviewer.courses_attached)
+lecturer.courses_attached += ['Python', 'C++']
+reviewer.courses_attached += ['Python', 'C++']
+
+print(student.rate_lecture(lecturer, 'Python', 7))   # None
+print(student.rate_lecture(lecturer, 'Java', 8))     # Ошибка
+print(student.rate_lecture(lecturer, 'C++', 8))      # Ошибка
+print(student.rate_lecture(reviewer, 'Python', 6))   # Ошибка
+
+print(lecturer.grades)  # {'Python': [7]}
