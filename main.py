997b72cc797e0acd522e7a593a91bac347253177
p4jjhjjkvj-blip <lexa_2000@mail.ru>
2026-06-13 20:17:11 +0300

    Добавлена оценка лекторам

--- main.py
+++ main.py
@@ -20,16 +20,19 @@ class Student:
         else:
             return 'Ошибка'
 
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
 
+
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
